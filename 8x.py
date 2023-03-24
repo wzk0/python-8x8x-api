70faae5e595c10169936c173d54381fb3b2e7689
wzk0 <729301277@qq.com>
2023-03-24 23:09:20 +0800

    添加了搜索功能

--- 8x.py
+++ 8x.py
@@ -3,7 +3,7 @@ import os
 import json
 from bs4 import BeautifulSoup
 
-domain="https://81xaod.top"
+domain="https://8018ow.top"
 update="https://github.com/8xx8x/8x8x"
 
 def get_data(page):
@@ -15,6 +15,7 @@ def get_data(page):
 def analysis_page(html):
     soup=BeautifulSoup(open("temp.html"),features="html5lib")
     for page in soup.find_all("a"):
+        print(page)
         if "末页" in page:
             last_page=BeautifulSoup(str(page),features="html5lib").a["href"].split("/")[-2]
     ul=soup.find_all("li")
@@ -22,15 +23,11 @@ def analysis_page(html):
     for u in ul:
         if "<a href=\"/video/" in str(u):
             new_ul.append(BeautifulSoup(str(u),features="html5lib").p)
-    names=[]
-    urls=[]
-    tags=[]
+    data=[]
     for ll in new_ul[1:-1]:
         new_ll=BeautifulSoup(str(ll),features="html5lib")
-        names.append("".join(new_ll.p.string.split("-")[:-1]))
-        urls.append(domain+new_ll.a["href"])
-        tags.append(new_ll.p.string.split("-")[-1])
-    return names,urls,tags,last_page
+        data.append({'name':"".join(new_ll.p.string.split("-")[:-1]),'url':domain+new_ll.a["href"],'tag':new_ll.p.string.split("-")[-1]})
+    return data,last_page
 
 def analysis_video(href):
     soup=BeautifulSoup(requests.get(domain+"/video/%s/"%href).text,features="html5lib")
@@ -40,13 +37,18 @@ def analysis_video(href):
     for li in soup.find_all("li"):
         if ".mp4" in str(li):
             url=BeautifulSoup(str(li),features="html5lib").a["href"]
-            return "".join(aname.split("-")[:-1]),url,aname.split("-")[-1]
+    return {'name':"".join(aname.split("-")[:-1]),'url':domain+'/video/%s'%href,'id':href,'dl':url,'tag':aname.split("-")[-1]}
+
+def search(word,page):
+    r=requests.post('https://s.%s/search'%domain.replace('https://',''),headers={'Content-type':'application/x-www-form-urlencoded'},data={'title':str(word),'current':str(page),'size':'16','source':'v1'})
+    data=json.loads(r.text)
+    return data['data'],data['totalPage'],data['page']
 
 def star(href):
-    name,url,tag=analysis_video(href)
+    da=analysis_video(href)
     with open("data.json","r")as data:
         data=json.loads(data.read())
-        data.append({"name":name,"url":domain+"/video/%s"%href,"dl":url,"tag":tag,"id":href})
+        data.append(da)
     with open("data.json","w")as new_data:
         new_data.write(json.dumps(data,ensure_ascii=False))
 
@@ -55,7 +57,7 @@ def show_star():
         for d in json.loads(data.read()):
             print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n├── 下载: \033[1;32m%s\033[0m\n├── 标签: \033[1;31m%s\033[0m\n└── ID: \033[1;36m%s\033[0m\n"%(d["name"],d["url"],d["dl"],d["tag"],d["id"]))
 
-dashboard=["浏览页面","获取视频","收藏视频","展示收藏"]
+dashboard=["浏览页面","获取视频",'搜索',"收藏视频","展示收藏"]
 for dash in dashboard:
     print(str(dashboard.index(dash))+". "+dash)
 mode=int(input("请输入序号以继续:"))
@@ -64,18 +66,24 @@ if mode==0:
     data=get_data(pid)
     with open("temp.html","w")as f:
         f.write(data)
-    names,urls,tags,last_page=analysis_page(data)
+    data,last_page=analysis_page(data)
     os.system("clear")
     print("当前页面: %s - 共有页面: %s\n"%(pid,last_page))
-    for n,u,t in zip(names,urls,tags):
-        print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n├── 标签: \033[1;31m%s\033[0m\n└── ID: \033[1;36m%s\033[0m\n"%(n,u,t,u.split("/")[-2]))
+    for d in data:
+        print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n├── 标签: \033[1;31m%s\033[0m\n└── ID: \033[1;36m%s\033[0m\n"%(d['name'],d['url'],d['tag'],d['url'].split("/")[-2]))
 if mode==1:
-    name,url,tag=analysis_video(input("请输入视频ID:"))
+    data=analysis_video(input("请输入视频ID:"))
     os.system("clear")
-    print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n└── 标签: \033[1;31m%s\033[0m\n"%(name,url,tag))                                  
+    print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n└── 标签: \033[1;31m%s\033[0m\n"%(data['name'],data['dl'],data['tag']))                                  
 if mode==2:
-    star(input("请输入视频ID:"))
+    data,last_page,now_page=search(input('请输入关键词:'),input('请输入搜索页码(不确定请输入1):'))
+    os.system("clear")
+    print("当前页面: %s - 共有页面: %s\n"%(now_page,last_page))
+    for d in data:
+        print('\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n├── 标签: \033[1;31m%s\033[0m\n└── ID: \033[1;36m%s\033[0m\n'%(''.join(d['videoTitle'].split('-')[:-1]),domain+d['pageUrl'],d['videoTitle'].split('-')[-1],d['videoInfoId']))
 if mode==3:
+    star(input("请输入视频ID:"))
+if mode==4:
     show_star()
 else:
     print("Bye")
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # 不多说了...
 
-![](https://github.com/wzk0/photo/blob/main/Screenshot_20230319_145932_com.termux_edit_27598273968704.jpg?raw=true)
+[屏幕截图](https://github.com/wzk0/photo/blob/main/Screenshot_20230319_145932_com.termux_edit_27598273968704.jpg?raw=true)
 
 > 一个避免直接看片被发现后处境尴尬以及误点广告几率太高的小工具!
 
@@ -8,7 +8,8 @@
 
 1. 获取页面上的内容(片名, ID, 标签);
 2. 获取视频直链;
-3. 收藏和展示收藏.
+3. 收藏和展示收藏;
+4. 搜索.
 
 片库: https://github.com/8xx8x/8x8x
 
