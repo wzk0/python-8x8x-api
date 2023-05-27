abac0e8744ae3c8225798175c69deb3284586d42
wzk0 <729301277@qq.com>
2023-05-27 23:32:17 +0800

    改变方向 搞API!

--- 8x.py
+++ 8x.py
@@ -1,89 +1,46 @@
 import requests
-import os
-import json
 from bs4 import BeautifulSoup
+import json
 
-domain="https://8018ow.top"
+headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'}
+
+domain="https://8vcsh.top"
 update="https://github.com/8xx8x/8x8x"
 
-def get_data(page):
-    if page==1:
-        return requests.get(domain+"/video").text
+def req(link):
+    r=requests.get(link,headers=headers)
+    soup=BeautifulSoup(r.text,features='html5lib')
+    return soup
+
+def analysis_page(page):
+    if int(page)==1:
+        soup=req(domain+'/video/')
     else:
-        return requests.get(domain+"/video/page/%s/"%page).text
+        soup=req(domain+'/video/page/%s/'%page)
+    all_info=[]
+    for li in soup.find_all('li'):
+        if '点击在线播放' in str(li): 
+            all_info.append({'name':li.find('p').text,'pic':li.find('img')['data-src'],'vid':li.find('a')['href'].split('/')[-2]})
+    return all_info
 
-def analysis_page(html):
-    soup=BeautifulSoup(open("temp.html"),features="html5lib")
-    for page in soup.find_all("a"):
-        print(page)
-        if "末页" in page:
-            last_page=BeautifulSoup(str(page),features="html5lib").a["href"].split("/")[-2]
-    ul=soup.find_all("li")
-    new_ul=[]
-    for u in ul:
-        if "<a href=\"/video/" in str(u):
-            new_ul.append(BeautifulSoup(str(u),features="html5lib").p)
-    data=[]
-    for ll in new_ul[1:-1]:
-        new_ll=BeautifulSoup(str(ll),features="html5lib")
-        data.append({'name':"".join(new_ll.p.string.split("-")[:-1]),'url':domain+new_ll.a["href"],'tag':new_ll.p.string.split("-")[-1]})
-    return data,last_page
-
-def analysis_video(href):
-    soup=BeautifulSoup(requests.get(domain+"/video/%s/"%href).text,features="html5lib")
-    for h in soup.find_all("h1"):
-        if "lhgt" in str(h):
-            aname=BeautifulSoup(str(h),features="html5lib").h1.string
-    for li in soup.find_all("li"):
-        if ".mp4" in str(li):
-            url=BeautifulSoup(str(li),features="html5lib").a["href"]
-    return {'name':"".join(aname.split("-")[:-1]),'url':domain+'/video/%s'%href,'id':href,'dl':url,'tag':aname.split("-")[-1]}
+def analysis_video(vid):
+    soup=req('https://8vcsh.top/video/%s/'%vid)
+    def give_me_soup(soup):
+        for div in soup.find_all('div'):
+            if '下载观看' in str(div):
+                return div
+    div=give_me_soup(soup)
+    for a in div.find_all('a'):
+        if '下载观看' in str(a):
+            download=a['href']
+    for h1 in soup.find_all('h1'):
+        if 'lhgt' in str(h1):
+            name=h1.text
+    return {'name':name,'background':soup.find('video')['poster'],'download':download}
 
 def search(word,page):
     r=requests.post('https://s.%s/search'%domain.replace('https://',''),headers={'Content-type':'application/x-www-form-urlencoded'},data={'title':str(word),'current':str(page),'size':'16','source':'v1'})
     data=json.loads(r.text)
     return data['data'],data['totalPage'],data['page']
 
-def star(href):
-    da=analysis_video(href)
-    with open("data.json","r")as data:
-        data=json.loads(data.read())
-        data.append(da)
-    with open("data.json","w")as new_data:
-        new_data.write(json.dumps(data,ensure_ascii=False))
-
-def show_star():
-    with open("data.json","r")as data:
-        for d in json.loads(data.read()):
-            print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n├── 下载: \033[1;32m%s\033[0m\n├── 标签: \033[1;31m%s\033[0m\n└── ID: \033[1;36m%s\033[0m\n"%(d["name"],d["url"],d["dl"],d["tag"],d["id"]))
-
-dashboard=["浏览页面","获取视频",'搜索',"收藏视频","展示收藏"]
-for dash in dashboard:
-    print(str(dashboard.index(dash))+". "+dash)
-mode=int(input("请输入序号以继续:"))
-if mode==0:
-    pid=int(input("请输入页面序号:"))
-    data=get_data(pid)
-    with open("temp.html","w")as f:
-        f.write(data)
-    data,last_page=analysis_page(data)
-    os.system("clear")
-    print("当前页面: %s - 共有页面: %s\n"%(pid,last_page))
-    for d in data:
-        print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n├── 标签: \033[1;31m%s\033[0m\n└── ID: \033[1;36m%s\033[0m\n"%(d['name'],d['url'],d['tag'],d['url'].split("/")[-2]))
-if mode==1:
-    data=analysis_video(input("请输入视频ID:"))
-    os.system("clear")
-    print("\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n└── 标签: \033[1;31m%s\033[0m\n"%(data['name'],data['dl'],data['tag']))                                  
-if mode==2:
-    data,last_page,now_page=search(input('请输入关键词:'),input('请输入搜索页码(不确定请输入1):'))
-    os.system("clear")
-    print("当前页面: %s - 共有页面: %s\n"%(now_page,last_page))
-    for d in data:
-        print('\033[1;33m%s\033[0m\n├── 链接: \033[1;35m%s\033[0m\n├── 标签: \033[1;31m%s\033[0m\n└── ID: \033[1;36m%s\033[0m\n'%(''.join(d['videoTitle'].split('-')[:-1]),domain+d['pageUrl'],d['videoTitle'].split('-')[-1],d['videoInfoId']))
-if mode==3:
-    star(input("请输入视频ID:"))
-if mode==4:
-    show_star()
-else:
-    print("Bye")
+print(analysis_video(87981))
--- readme.md
+++ readme.md
@@ -1,46 +1,67 @@
-# 不多说了...
+# python-8x8x-api
 
-[屏幕截图](https://github.com/wzk0/photo/blob/main/Screenshot_20230319_145932_com.termux_edit_27598273968704.jpg?raw=true)
-
-> 一个避免直接看片被发现后处境尴尬以及误点广告几率太高的小工具!
-
-支持:
-
-1. 获取页面上的内容(片名, ID, 标签);
-2. 获取视频直链;
-3. 收藏和展示收藏;
-4. 搜索.
+一个 8x8x (没错就是那个) 的 python API !
 
 片库: https://github.com/8xx8x/8x8x
 
-## 使用
 
-clone此仓库:
 
-```sh
-git clone https://github.com/wzk0/8x
+# 支持
+
+* 获取页面上的所有视频信息(封面, 标题, 视频ID);
+
+* 获取单个视频的信息(视频下载链接, 封面, 标题);
+
+* 搜索(网站的问题导致搜索可能无法使用).
+
+
+# 介绍
+
+1. `analysis_page`: 获取指定页面的所有视频信息
+
+返回结果如下:
+
+```json
+[
+    {
+        "name": "美xxxxx刚xx完就xx要xxxxxxxxx-xxxx",	//视频标题
+        "pic": "https://h2bccytq4w.ng8wu.com/p2/46xxxxxxxxf15f8b9826fd7908d44098.webp",	//视频封面
+        "vid": "88366"	//视频ID
+    },
+    {
+        "name": "热门推荐-十年来未见面xxxxx越来越xx-家庭xx",
+        "pic": "https://h2bccytq4w.ng8wu.com/p2/f6xxxxxxxxe0bb62666bb801fe58cd0e.webp",
+        "vid": "88239"
+    }
+]
 ```
 
-安装`requests`, `html5lib`和`beautifulsoup4`:
+需要传入`page`参数.(`page`是页码数)
 
-```sh
-pip install beautifulsoup4 html5lib requests
+---
+
+2. `analysis_video`: 获取指定视频的信息
+
+返回结果如下:
+
+```json
+{
+    "name": "国产xx：xxxxxxxxxx-年xxx",	//视频标题
+    "background": "https://h2bccytq4w.ng8wu.com/p2/23cxxxxxxxxx87539a15cb2befd.webp",	//视频封面
+    "download": "https://8xj8j.xyz/assets/23c7xxxxxxxxxxx39a15cb2befd.mp4"	//视频下载链接
+}
 ```
 
-运行:
+需要传入`vid`参数.(`vid`是视频ID)
 
-```sh
-python3 8x.py
-```
+---
 
-接着按照序号操作提示即可.
+3. `search`: 搜索
+
+返回结果未知.(网站的这个搜索功能老是失效)
+
+需要传入`word`, `page`两个参数.(`word`是搜索关键词)
 
 ## 情况
 
-* 如果失效的话请去片库仓库自行获取链接替换程序中的`domain`参数;
-
-* 使用搜索功能时, 如果报错说明需要代理.
-
-## 其他
-
-实际上, 在稍微修改源代码后, 也可以充当8x8x的一个API(不过我猜没有这个需求, 所以不搞了).
+* 如果失效的话请去片库仓库自行获取链接替换程序中的`domain`参数.
