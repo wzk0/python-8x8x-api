0236cf9f346ed29872c98b6d49d0ddd5ffcee46c
wzk0 <729301277@qq.com>
2023-06-04 22:02:32 +0800

    加入标签功能

--- 8x.py
+++ 8x.py
@@ -4,14 +4,16 @@ import json
 
 headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'}
 
-domain="https://8vcsh.top"
+domain="https://8gpph.top"
 update="https://github.com/8xx8x/8x8x"
 
+# 通用请求
 def req(link):
     r=requests.get(link,headers=headers)
     soup=BeautifulSoup(r.text,features='html5lib')
     return soup
 
+# 分析视频页面
 def analysis_page(page):
     if int(page)==1:
         soup=req(domain+'/video/')
@@ -23,8 +25,9 @@ def analysis_page(page):
             all_info.append({'name':li.find('p').text,'pic':li.find('img')['data-src'],'vid':li.find('a')['href'].split('/')[-2]})
     return all_info
 
+# 分析视频
 def analysis_video(vid):
-    soup=req('https://8vcsh.top/video/%s/'%vid)
+    soup=req(domain+'/video/%s/'%vid)
     def give_me_soup(soup):
         for div in soup.find_all('div'):
             if '下载观看' in str(div):
@@ -36,11 +39,30 @@ def analysis_video(vid):
     for h1 in soup.find_all('h1'):
         if 'lhgt' in str(h1):
             name=h1.text
-    return {'name':name,'background':soup.find('video')['poster'],'download':download}
+    return {'name':name,'pic':soup.find('video')['poster'],'download':download,'vid':str(vid)}
 
+# 获取标签
+def get_tags():
+    soup=req(domain+'/tags/')
+    all_info=[]
+    for a in soup.find_all('a',class_='semanimg'):
+        all_info.append(a.find('img').get('alt'))
+    return all_info
+
+# 获取标签页面
+def analysis_tag(tag,page):
+    if int(page)==1:
+        soup=req(domain+'/tags/%s'%tag)
+    else:
+        soup=req(domain+'/tags/%s/page/%s/'%(tag,page))
+    all_info=[]
+    for li in soup.find_all('li'):
+        if '点击在线播放' in str(li): 
+            all_info.append({'name':li.find('p').text,'pic':li.find('img')['data-src'],'vid':li.find('a')['href'].split('/')[-2]})
+    return all_info
+
+# 搜索
 def search(word,page):
     r=requests.post('https://s.%s/search'%domain.replace('https://',''),headers={'Content-type':'application/x-www-form-urlencoded'},data={'title':str(word),'current':str(page),'size':'16','source':'v1'})
     data=json.loads(r.text)
     return data['data'],data['totalPage'],data['page']
-
-print(analysis_video(87981))
--- readme.md
+++ readme.md
@@ -5,12 +5,15 @@
 片库: https://github.com/8xx8x/8x8x
 
 
-
 # 支持
 
 * 获取页面上的所有视频信息(封面, 标题, 视频ID);
 
 * 获取单个视频的信息(视频下载链接, 封面, 标题);
+
+* 获取标签列表;
+
+* 获取标签下的视频;
 
 * 搜索(网站的问题导致搜索可能无法使用).
 
@@ -19,49 +22,34 @@
 
 1. `analysis_page`: 获取指定页面的所有视频信息
 
-返回结果如下:
-
-```json
-[
-    {
-        "name": "美xxxxx刚xx完就xx要xxxxxxxxx-xxxx",	//视频标题
-        "pic": "https://h2bccytq4w.ng8wu.com/p2/46xxxxxxxxf15f8b9826fd7908d44098.webp",	//视频封面
-        "vid": "88366"	//视频ID
-    },
-    {
-        "name": "热门推荐-十年来未见面xxxxx越来越xx-家庭xx",
-        "pic": "https://h2bccytq4w.ng8wu.com/p2/f6xxxxxxxxe0bb62666bb801fe58cd0e.webp",
-        "vid": "88239"
-    }
-]
-```
-
 需要传入`page`参数.(`page`是页码数)
 
 ---
 
 2. `analysis_video`: 获取指定视频的信息
 
-返回结果如下:
-
-```json
-{
-    "name": "国产xx：xxxxxxxxxx-年xxx",	//视频标题
-    "background": "https://h2bccytq4w.ng8wu.com/p2/23cxxxxxxxxx87539a15cb2befd.webp",	//视频封面
-    "download": "https://8xj8j.xyz/assets/23c7xxxxxxxxxxx39a15cb2befd.mp4"	//视频下载链接
-}
-```
-
 需要传入`vid`参数.(`vid`是视频ID)
 
 ---
 
-3. `search`: 搜索
+3. `get_tags`: 获取完整的标签列表
 
-返回结果未知.(网站的这个搜索功能老是失效)
+无需传入参数.
+
+---
+
+4. `analysis_tag`: 获取指定标签页面下的所有视频信息
+
+需要传入`tag`, `page`两个参数.(`tag`是标签名, 可通过`get_tags`获得)
+
+---
+
+5. `search`: 搜索
 
 需要传入`word`, `page`两个参数.(`word`是搜索关键词)
 
 ## 情况
 
 * 如果失效的话请去片库仓库自行获取链接替换程序中的`domain`参数.
+
+* 若要导入请将`8x.py`文件重命名为`x8或其他英文开头`的文件名''
